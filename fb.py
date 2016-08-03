1592edc90951d2d55ef71ff6edffbd45cb041cf5
Sandra Chung <sandra@moveon.org>
2016-08-03 16:41:54 -0600

    separate token getting from data retrieval

--- fb.py
+++ fb.py
@@ -43,18 +43,3 @@ def get_total_reach(posts_dict):
             total_reach = total_reach_values['data'][0]['values'][0]['value']
         posts_dict[post_id].append(total_reach)
     return posts_dict
-
-def get_long_lived_token():
-    user_access_token = 'add your user access token here'
-    client_id = 'add your client id here'
-    client_secret = 'add your client secret here'
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' %(client_id, client_secret, user_access_token)
-    token = requests.get(url)
-    return token.text[13:]
-
-def get_page_access_token_from_user_token(long_user_access_token):
-    url = base_url + '%s?fields=access_token&access_token=%s' %(fb_page_id, long_user_access_token)
-    page_token = requests.get(url).json()   
-    print(page_token['access_token'])
-    
-# get_page_access_token_from_user_token(get_long_lived_token())
