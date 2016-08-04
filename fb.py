96808735c2142d27dfb50becbcad9fd65062db45
Sandra Chung <sandra@moveon.org>
2016-08-04 15:14:09 -0600

    made requests more specific so can query more posts at a time

--- fb.py
+++ fb.py
@@ -9,27 +9,37 @@ import time
 base_url = "https://graph.facebook.com/%s/" %fb_version
 
 # this fn results in posts_dict = {post_id: [message, created_time, likes, shares, comments]}
+# set interval = False to update all post data
+
 def get_posts_and_interactions(interval='month'):
     
     now = int(time.time())
     if interval == 'week':
-        since = now - 604800
+        since = str(now - 604800)
     if interval == 'month':
-        since = now - 2592000
+        since = str(now - 2592000)
     if interval == 'year':
-        since = now - 31536000
+        since = str(now - 31536000)
+    now = str(now)
     
     limit = post_limit
     too_many_posts_at_a_time = True
 
     while too_many_posts_at_a_time:
         if interval:
-            url = base_url + '%s/posts?fields=message,created_time,id,likes.summary(true),comments.summary(true),shares&limit=%s&since=%s&access_token=%s' %(fb_page_id, limit, since, fb_long_token)
+            url = base_url + '%s/posts?fields=message,created_time,id,likes.limit(0).summary(total_count),comments.limit(0).summary(total_count),shares&limit=%s&since=%s&until=%s&access_token=%s' %(fb_page_id, limit, since, now, fb_long_token)
         else:
-            url = base_url + '%s/posts?fields=message,created_time,id,likes.summary(true),comments.summary(true),shares&limit=%s&access_token=%s' %(fb_page_id, limit, fb_long_token)
+            url = base_url + '%s/posts?fields=message,created_time,id,likes.limit(0).summary(true),comments.limit(0).summary(true),shares&limit=%s&access_token=%s' %(fb_page_id, limit, fb_long_token)
         posts = requests.get(url).json()
+
+        #debugging
+        out_file = open('posts.json','w') #debugging
+        json.dump(posts,out_file, indent=4) #debugging
+        #debugging
+
         if 'error' in posts:
-            limit = str(int(limit) - 5)
+            print(str(limit) + ' is too high')
+            limit = str(int(limit) - 5) # try again with a smaller request
         else:
             too_many_posts_at_a_time = False
 
@@ -63,14 +73,17 @@ def get_posts_and_interactions(interval='month'):
                     json.dump(post, error_log, indent=4)                    
             posts_dict[post['id']] = [message, created_time, likes, shares, comments]
         if 'paging' in posts:
+            print('next page')
             posts = requests.get(posts['paging']['next']).json()
         else:
+            print('last page')
             pagination = False
 
     return posts_dict
 
 
 # this fn results in posts_dict = {post_id: [message, created_time, likes, shares, comments, total_reach]}
+# TODO: rewrite this as a batch request https://developers.facebook.com/docs/graph-api/making-multiple-requests
 def get_total_reach(posts_dict):
     for post_id in posts_dict.keys():
         url = base_url + "%s/insights/post_impressions?period=lifetime&access_token=%s" %(post_id, fb_long_token)
@@ -80,3 +93,77 @@ def get_total_reach(posts_dict):
             total_reach = total_reach_values['data'][0]['values'][0]['value']
         posts_dict[post_id].append(total_reach)
     return posts_dict
+
+#avg_completion = percentage of video watched
+# this results in videos_dict = {video_id: [title, description, views, likes, reactions, comments, 10sec_views, 30sec_views, to_95_percent, length, avg_time_watched, viewers, unique_viewers]}
+
+def get_video_stats(interval = False):
+    now = int(time.time())
+    if interval == 'week':
+        since = str(now - 604800)
+    if interval == 'month':
+        since = str(now - 2592000)
+    if interval == 'year':
+        since = str(now - 31536000)
+    now = str(now)
+
+    limit = post_limit
+    too_many_posts_at_a_time = True
+
+    while too_many_posts_at_a_time:
+        if interval:
+            url = base_url + '%s/videos?title,description,created_time,length,comments.limit(0),likes.limit(0),reactions.limit(0),video_insights{values,name}&limit=%s&since=%s&until=%s&access_token=%s' %(fb_page_id, limit, since, now, fb_long_token)
+        else:
+            url = base_url + '%s/videos?title,description,created_time,length,comments.limit(0),likes.limit(0),reactions.limit(0),video_insights{values,name}&limit=%s&access_token=%s' %(fb_page_id, limit, fb_long_token)
+        videos = requests.get(url).json()
+        if 'error' in videos:
+            print(str(limit) + ' is too high')
+            limit = str(int(limit) - 5) # try again with a smaller request
+        else:
+            too_many_posts_at_a_time = False
+
+    videos_dict = {}
+
+    out_file = open('videos.json','w')
+    json.dump(videos,out_file, indent=4)
+
+    # pagination = True
+
+    # while pagination:
+    #     for post in posts['data']:
+    #         message = post.get('message', '').replace('\n', ' ').replace(',', ' ')
+    #         created_time = post['created_time'].replace('T', ' ').replace('+0000', '')
+    #         likes = 0
+    #         shares = 0
+    #         comments = 0
+    #         if 'likes' in post:
+    #             try:
+    #                 likes = post['likes']['summary']['total_count']
+    #             except KeyError:
+    #                 error_log = open('error_log.json','a')
+    #                 json.dump(post, error_log, indent=4)
+    #         if 'shares' in post:
+    #             try:
+    #                 shares = post['shares']['count']
+    #             except KeyError:
+    #                 error_log = open('error_log.json','a')
+    #                 json.dump(post, error_log, indent=4)
+    #         if 'comments' in post:
+    #             try:
+    #                 comments = post['comments']['summary']['total_count']
+    #             except KeyError:
+    #                 error_log = open('error_log.json','a')
+    #                 json.dump(post, error_log, indent=4)                    
+    #         posts_dict[post['id']] = [message, created_time, likes, shares, comments]
+    #     if 'paging' in posts:
+    #         posts = requests.get(posts['paging']['next']).json()
+    #     else:
+    #         pagination = False
+
+    return videos_dict
+
+# TODO: get shares and reactions separately in a batch request, count them, and add them to the dict
+# TODO: get reach for all the videos
+
+#for dev only
+#get_video_stats()
--- fb_to_redshift.py
+++ fb_to_redshift.py
@@ -5,10 +5,11 @@ from fb import get_posts_and_interactions, get_total_reach
 from settings import aws_access_key, aws_secret_key, s3_bucket
 import boto
 import csv
+import argparse
 
-def create_import_file():
+def create_import_file(interval='month'):
     import_file = open('fb_import_file.csv', 'w')
-    fb_dict = get_total_reach(get_posts_and_interactions('month'))
+    fb_dict = get_total_reach(get_posts_and_interactions(interval))
     csv_file = csv.writer(import_file, quoting=csv.QUOTE_MINIMAL)
     csv_file.writerows([[post_id,]+post_values for post_id, post_values in fb_dict.items()])
     import_file.close()
