ca090550fe43c238e391de52b02c6a281f6f1372
Sandra Chung <sandra@moveon.org>
2016-10-19 20:29:36 -0600

    refactored and added filter by list id

--- fb_to_redshift.py
+++ fb_to_redshift.py
@@ -7,16 +7,16 @@ import boto
 import csv
 import argparse
 
-def create_import_file(interval=False, import_type='posts'):
+def create_import_file(interval=False, import_type='posts', filename='fb_import_posts.csv', list_id = None):
     if import_type == 'posts':
-        import_file = open('fb_import_posts.csv', 'w')
+        import_file = open(filename, 'w')
         data_dict = get_total_reach(get_posts_and_interactions(interval))
     if import_type == 'videos':
-        import_file = open('fb_import_videos.csv', 'w')
+        import_file = open(filename, 'w')
         data_dict = get_video_stats(interval)
-    if import_type == 'video_lab':
-        import_file = open('fb_import_video_lab.csv', 'w')
-        data_dict = get_video_stats(interval,True)
+    if import_type == 'video_lab_videos':
+        import_file = open(filename, 'w')
+        data_dict = get_video_stats(interval, True, list_id)
     csv_file = csv.writer(import_file, quoting=csv.QUOTE_MINIMAL)
     csv_file.writerows([[id,]+values for id, values in data_dict.items()])
     import_file.close()
@@ -28,104 +28,89 @@ def upload_to_s3(filename='fb_import_posts.csv'):
     k.key = filename
     k.set_contents_from_filename(filename)
 
-def update_redshift_posts():
+def update_redshift(table_name, columns, primary_key, filename):
+    staging_table_name = table_name + "_staging"
+    column_names = ", ".join(columns)
+    columns_to_stage = ", ".join([(column + " = s." + column) for column in columns ])
+    table_key = table_name + "." + primary_key
+    staging_table_key = "s." + primary_key
+
     command = """-- Create a staging table 
-CREATE TABLE facebook.posts_staging (LIKE facebook.posts);
+CREATE TABLE %s (LIKE %s);
 
 -- Load data into the staging table 
-COPY facebook.posts_staging (post_id, message, created_time, likes, shares, comments, total_reach) 
-FROM 's3://%s/fb_import_posts.csv' 
-CREDENTIALS 'aws_access_key_id=%s;aws_secret_access_key=%s'
-delimiter ','; 
-
--- Update records 
-UPDATE facebook.posts
-SET message = s.message, created_time = s.created_time, likes = s.likes, shares = s.shares, comments = s.comments, total_reach = s.total_reach
-FROM facebook.posts_staging s 
-WHERE facebook.posts.post_id = s.post_id; 
-
--- Insert records 
-INSERT INTO facebook.posts
-SELECT s.* FROM facebook.posts_staging s LEFT JOIN facebook.posts
-ON s.post_id = facebook.posts.post_id
-WHERE facebook.posts.post_id IS NULL;
-
--- Drop the staging table
-DROP TABLE facebook.posts_staging; 
-
--- End transaction 
-END;"""%(s3_bucket, aws_access_key, aws_secret_key)
-
-    rsm.db_query(command)
-
-def update_redshift_videos():
-    command = """-- Create a staging table 
-CREATE TABLE facebook.videos_staging (LIKE facebook.videos);
-
--- Load data into the staging table 
-COPY facebook.videos_staging (video_id, title, description, created_time, video_length, likes, comments, reactions, shares, reach, ms_viewed, total_views, unique_viewers, views_10sec, views_30sec, views_95pct, avg_completion) 
-FROM 's3://%s/fb_import_videos.csv' 
+COPY %s (%s) 
+FROM 's3://%s/%s' 
 CREDENTIALS 'aws_access_key_id=%s;aws_secret_access_key=%s'
 FILLRECORD
 delimiter ','; 
 
 -- Update records 
-UPDATE facebook.videos 
-SET title = s.title, description = s.description, created_time = s.created_time, video_length = s.video_length, likes = s.likes, comments = s.comments, reactions = s.reactions, shares = s.shares, reach = s.reach, ms_viewed = s.ms_viewed, total_views = s.total_views, unique_viewers = s.unique_viewers, views_10sec = s.views_10sec, views_30sec = s.views_30sec, views_95pct = s.views_95pct, avg_completion = s.avg_completion
-FROM facebook.videos_staging s
-WHERE facebook.videos.video_id = s.video_id; 
+UPDATE %s
+SET %s
+FROM %s s
+WHERE %s = %s; 
 
 -- Insert records 
-INSERT INTO facebook.videos 
-SELECT s.* FROM facebook.videos_staging s LEFT JOIN facebook.videos 
-ON s.video_id = facebook.videos.video_id
-WHERE facebook.videos.video_id IS NULL;
+INSERT INTO %s 
+SELECT s.* FROM %s s LEFT JOIN %s 
+ON %s = %s
+WHERE %s IS NULL;
 
 -- Drop the staging table
-DROP TABLE facebook.videos_staging; 
+DROP TABLE %s; 
 
 -- End transaction 
-END;"""%(s3_bucket, aws_access_key, aws_secret_key)
+END;"""%(staging_table_name, table_name, staging_table_name, column_names, s3_bucket, filename, aws_access_key, aws_secret_key, table_name, columns_to_stage, staging_table_name, table_key, staging_table_key, table_name, staging_table_name, table_name, staging_table_key, table_key, table_key, staging_table_name )
 
     rsm.db_query(command)
+    return
 
-def update_redshift_video_lab_videos():
-    command = """-- Create a staging table 
-CREATE TABLE facebook.video_lab_staging (LIKE facebook.video_lab_videos);
+posts = {}
+posts['interval'] = 'month'
+posts['import_type'] = 'posts'
+posts['filename'] = 'fb_import_posts.csv'
+posts['tablename'] = 'facebook.posts'
+posts['columns'] = ['post_id', 'message', 'created_time', 'likes', 'shares', 'comments', 'total_reach']
+posts['primary_key'] = 'post_id'
 
--- Load data into the staging table 
-COPY facebook.video_lab_staging (video_id, title, description, created_time, video_length, likes, comments, reactions, shares, reach, ms_viewed, total_views, unique_viewers, views_10sec, views_30sec, views_95pct, avg_completion) 
-FROM 's3://%s/fb_import_video_lab.csv' 
-CREDENTIALS 'aws_access_key_id=%s;aws_secret_access_key=%s'
-FILLRECORD
-delimiter ','; 
+videos = {}
+videos['interval'] = False
+videos['import_type'] = 'videos'
+videos['filename'] = 'fb_import_videos.csv'
+videos['tablename'] = 'facebook.videos'
+videos['columns'] = ['video_id', 'title', 'description', 'created_time', 'video_length', 'likes', 'comments', 'reactions', 'shares', 'reach', 'ms_viewed ', 'total_views', 'unique_viewers', 'views_10sec', 'views_30sec', 'views_95pct', 'avg_completion']
+videos['primary_key'] = 'video_id'
 
--- Update records 
-UPDATE facebook.video_lab_videos 
-SET title = s.title, description = s.description, created_time = s.created_time, video_length = s.video_length, likes = s.likes, comments = s.comments, reactions = s.reactions, shares = s.shares, reach = s.reach, ms_viewed = s.ms_viewed, total_views = s.total_views, unique_viewers = s.unique_viewers, views_10sec = s.views_10sec, views_30sec = s.views_30sec, views_95pct = s.views_95pct, avg_completion = s.avg_completion
-FROM facebook.video_lab_staging s
-WHERE facebook.video_lab_videos.video_id = s.video_id; 
+video_lab_videos = {}
+video_lab_videos['interval'] = False
+video_lab_videos['import_type'] = 'video_lab_videos'
+video_lab_videos['filename'] = 'fb_import_video_lab.csv'
+video_lab_videos['tablename'] = 'facebook.video_lab_videos'
+video_lab_videos['columns'] = ['video_id', 'title', 'description', 'created_time', 'video_length', 'likes', 'comments', 'reactions', 'shares', 'reach', 'ms_viewed ', 'total_views', 'unique_viewers', 'views_10sec', 'views_30sec', 'views_95pct', 'avg_completion']
+video_lab_videos['primary_key'] = 'video_id'
+video_lab_videos['list_id'] = '1563848167245359'
 
--- Insert records 
-INSERT INTO facebook.video_lab_videos 
-SELECT s.* FROM facebook.video_lab_staging s LEFT JOIN facebook.video_lab_videos 
-ON s.video_id = facebook.video_lab_videos.video_id
-WHERE facebook.video_lab_videos.video_id IS NULL;
+video_lab_videos_2 = {}
+video_lab_videos_2['interval'] = False
+video_lab_videos_2['import_type'] = 'video_lab_videos'
+video_lab_videos_2['filename'] = 'fb_import_video_lab_2.csv'
+video_lab_videos_2['tablename'] = 'facebook.video_lab_videos'
+video_lab_videos_2['columns'] = ['video_id', 'title', 'description', 'created_time', 'video_length', 'likes', 'comments', 'reactions', 'shares', 'reach', 'ms_viewed ', 'total_views', 'unique_viewers', 'views_10sec', 'views_30sec', 'views_95pct', 'avg_completion']
+video_lab_videos_2['primary_key'] = 'video_id'
+video_lab_videos_2['list_id'] = '1225720367451359'
 
--- Drop the staging table
-DROP TABLE facebook.video_lab_staging; 
+data_types = [posts, videos, video_lab_videos, video_lab_videos_2]
 
--- End transaction 
-END;"""%(s3_bucket, aws_access_key, aws_secret_key)
-
-    rsm.db_query(command)
-
-create_import_file('month', 'posts')
-create_import_file(False, 'videos')
-create_import_file(False, 'video_lab')
-upload_to_s3('fb_import_posts.csv')
-upload_to_s3('fb_import_videos.csv')
-upload_to_s3('fb_import_video_lab.csv')
-update_redshift_posts()
-update_redshift_videos()
-update_redshift_video_lab_videos()
+for item in data_types:
+    if 'list_id' in item:
+        create_import_file(item.get('interval'), item.get('import_type'), item.get('filename'), item.get('list_id'))
+    else:
+        create_import_file(item.get('interval'), item.get('import_type'), item.get('filename'))
+    print("created " + item.get('filename')) 
+    
+    upload_to_s3(item.get('filename'))
+    print("uploaded " + item.get('filename') + " to s3")
+    
+    update_redshift(item.get('tablename'), item.get('columns'), item.get('primary_key'), item.get('filename'))
+    print("updated redshift table " + item.get('tablename'))
