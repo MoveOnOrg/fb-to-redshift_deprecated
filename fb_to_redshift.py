b4e8c83e65b25b6ad60bbc074e6e3e777e2ae5ad
Sandra Chung <sandra@moveon.org>
2016-10-25 23:09:20 -0600

    removed test flags

--- fb_to_redshift.py
+++ fb_to_redshift.py
@@ -11,24 +11,24 @@ def main():
     posts = {}
     posts['interval'] = 'month'
     posts['import_type'] = 'posts'
-    posts['filename'] = 'fb_import_posts_Test.csv'
-    posts['tablename'] = 'facebook.posts_test'
+    posts['filename'] = 'fb_import_posts.csv'
+    posts['tablename'] = 'facebook.posts'
     posts['columns'] = ['post_id', 'message', 'created_time', 'likes', 'shares', 'comments', 'total_reach']
     posts['primary_key'] = 'post_id'
 
     videos = {}
     videos['interval'] = False
     videos['import_type'] = 'videos'
-    videos['filename'] = 'fb_import_videos_test.csv'
-    videos['tablename'] = 'facebook.videos_test'
+    videos['filename'] = 'fb_import_videos.csv'
+    videos['tablename'] = 'facebook.videos'
     videos['columns'] = ['video_id', 'title', 'description', 'created_time', 'video_length', 'likes', 'comments', 'reactions', 'shares', 'reach', 'ms_viewed ', 'total_views', 'unique_viewers', 'views_10sec', 'views_30sec', 'views_95pct', 'avg_completion']
     videos['primary_key'] = 'video_id'
 
     video_lab_videos = {}
     video_lab_videos['interval'] = False
     video_lab_videos['import_type'] = 'video_lab_videos'
-    video_lab_videos['filename'] = 'fb_import_video_lab_test.csv'
-    video_lab_videos['tablename'] = 'facebook.video_lab_videos_test'
+    video_lab_videos['filename'] = 'fb_import_video_lab.csv'
+    video_lab_videos['tablename'] = 'facebook.video_lab_videos'
     video_lab_videos['columns'] = ['video_id', 'title', 'description', 'created_time', 'video_length', 'likes', 'comments', 'reactions', 'shares', 'reach', 'ms_viewed ', 'total_views', 'unique_viewers', 'views_10sec', 'views_30sec', 'views_95pct', 'avg_completion']
     video_lab_videos['primary_key'] = 'video_id'
     video_lab_videos['list_id'] = '1563848167245359'
@@ -36,8 +36,8 @@ def main():
     video_lab_videos_2 = {}
     video_lab_videos_2['interval'] = False
     video_lab_videos_2['import_type'] = 'video_lab_videos'
-    video_lab_videos_2['filename'] = 'fb_import_video_lab_2_test.csv'
-    video_lab_videos_2['tablename'] = 'facebook.video_lab_videos_test'
+    video_lab_videos_2['filename'] = 'fb_import_video_lab_2.csv'
+    video_lab_videos_2['tablename'] = 'facebook.video_lab_videos'
     video_lab_videos_2['columns'] = ['video_id', 'title', 'description', 'created_time', 'video_length', 'likes', 'comments', 'reactions', 'shares', 'reach', 'ms_viewed ', 'total_views', 'unique_viewers', 'views_10sec', 'views_30sec', 'views_95pct', 'avg_completion']
     video_lab_videos_2['primary_key'] = 'video_id'
     video_lab_videos_2['list_id'] = '1225720367451359'
