4a1c70240a4a429b199975409f075a34d41cb636
Sandra Chung <sandra@moveon.org>
2016-12-22 10:50:01 -0700

    updated setup.py again so something to tag

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Sandra Chung,Rivkah Standig',
     author_email='opensource@moveon.org',
     packages=['fb-to-redshift'],
-    scripts=['fb-to-redshift/fb_get_token.py','fb-to-redshift/fb_to_redshift.py','fb-to-redshift/fb_video_time_series.py'],
+    scripts=['fb-to-redshift/fb_get_token.py','fb-to-redshift/fb_to_redshift.py','fb-to-redshift/fb_video_time_series.py','fb-to-redshift/fb.py','fb-to-redshift/redshift.py'],
     url='https://github.com/MoveOnOrg/fb-to-redshift',
     license='MIT',
     description="Download Facebook Page post and video data in CSV format, and import into Amazon Redshift tables.",
